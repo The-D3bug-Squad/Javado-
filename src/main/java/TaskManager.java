fe88252e7a1f1beed1b881b78738625e694a7b1a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-02-04 19:45:37 +0200

    Add method implementation.

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,19 +1,19 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
         // Initialize tasks list
+        this.tasks = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        throw new UnsupportedOperationException("Implement this method!");
+        this.tasks.add(task);
     }
 
-    public List<String> listTasks() {
-        throw new UnsupportedOperationException("Implement this method!");
-    }
+    public List<String> listTasks() { return this.tasks; }
 
     public void deleteTask(String task){
 //        leave for iteration 4

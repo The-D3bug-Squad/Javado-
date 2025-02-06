b1e2e64f670193603835522d16d7c783eb82e4a4
Thandeka Makamba <thmakamjhb024@student.wethinkcode.co.za>
2025-02-06 10:17:42 +0200

    Solution to iteration 1

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,18 +1,20 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
         // Initialize tasks list
+        tasks = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        throw new UnsupportedOperationException("Implement this method!");
+        tasks.add(task);
     }
 
     public List<String> listTasks() {
-        throw new UnsupportedOperationException("Implement this method!");
+        return tasks;
     }
 
     public void deleteTask(String task){

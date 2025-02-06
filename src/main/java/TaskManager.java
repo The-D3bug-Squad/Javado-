a173dbd2db1233d91e64d0d165d4b81704e4fd28
karabo <mawaiskara@gmail.com>
2025-02-06 10:36:56 +0200

    done

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
         // Initialize tasks list
-
+        this.tasks = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        tasks.add("do_dishes");
+        tasks.add(task);
     }
 
     public List<String> listTasks() {
         return tasks;
     }
+
 
     public void deleteTask(String task){
 //        leave for iteration 4

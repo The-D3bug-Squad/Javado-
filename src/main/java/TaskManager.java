8638ace330b77d64da5dde297404b5c7a2b158cc
Auston Mtabane <mtabane.auston@gmail.com>
2025-02-07 20:52:02 +0200

    added file writing and reading

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,5 +1,13 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.FileReader;
+import  java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
@@ -7,6 +15,19 @@ public class TaskManager {
     public TaskManager() {
         // Initialize tasks list
         tasks = new ArrayList<>();
+        try {
+            File file = new File("Tasks.txt");
+            Scanner scanner =new Scanner(file);
+
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                tasks.add(line);
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File not found");
+
+        }
+
     }
 
     public void addTask(String task) {
@@ -24,6 +45,17 @@ public class TaskManager {
     }
 
     public void exit() {
-        // leave for iteration 2
+        try {
+            FileWriter writer = new FileWriter("Tasks.txt");
+            for (String task : tasks) {
+                writer.write(task+"\n");
+
+            }
+
+            writer.close();
+        }catch (IOException e){
+            System.out.println("Error writing file .txt");
+        }
+//        Runtime.getRuntime().halt(0);
     }
 }

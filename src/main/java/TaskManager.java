7b5488171f964a8eae9799cc401749b463c33262
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2025-02-14 10:36:48 +0200

    feat(enhancement): Task added to file and persistent.

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,16 +1,39 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
+    File fileObj = new File("tasks.csv");
 
     public TaskManager() {
         // Initialize tasks list
         this.tasks = new ArrayList<>();
+        try {
+            this.fileObj.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error creating file");
+            e.printStackTrace();
+        }
     }
 
     public void addTask(String task) {
         this.tasks.add(task);
+        try {
+            FileWriter fwObj = new FileWriter("tasks.csv", true);
+            BufferedWriter bwObj = new BufferedWriter(fwObj);
+            bwObj.write(task);
+            bwObj.newLine();
+            bwObj.close();
+            fwObj.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file");
+            e.printStackTrace();
+        }
+
     }
 
     public List<String> listTasks() { return this.tasks; }
@@ -20,6 +43,6 @@ public class TaskManager {
     }
 
     public void exit() {
-        // leave for iteration 2
+        System.exit(0);
     }
 }

edd2b3b0f64b7fe66769384baa044022f5816533
namotswjhb024 <namotswjhb024@student.wethinkcode.co.za>
2025-02-17 14:32:54 +0200

    Iteration 2 Complete

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
+import java.nio.file.*;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
+    private static final String FILENAME = "tasks.csv";
 
     public TaskManager() {
         // Initialize tasks list
         this.tasks = new ArrayList<>();
+        loadTasksFromFile();
+        createFileIfNotExists();
     }
 
     public void addTask(String task) {
@@ -24,6 +29,43 @@ public class TaskManager {
     }
 
     public void exit() {
-        // leave for iteration 2
+        saveTasksToFile();
+    }
+
+    private void createFileIfNotExists() {
+        try{
+            File file = new File(FILENAME);
+            if(!file.exists()){
+                if(file.createNewFile()){
+                    System.out.println("File created: " + FILENAME);
+                }
+
+            }
+        }catch(Exception e){
+            System.out.println("Error creating file: " + e.getMessage());
+        }
+    }
+
+    private void loadTasksFromFile() {
+        try {
+            Path path = Paths.get(FILENAME);
+            if (!Files.exists(path)) {
+                tasks = Files.readAllLines(path);
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println("Error loading tasks from file: " + e.getMessage());
+        }
+    }
+    private void saveTasksToFile() {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))){
+            for (String task : tasks) {
+                writer.write(task);
+                writer.newLine();
+            }
+        }catch (IOException e) {
+            System.out.println("Error saving tasks to file: " + e.getMessage());
+        }
     }
 }

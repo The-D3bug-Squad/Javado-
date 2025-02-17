5b7f937816179a6a7a608340da54acb234b7bc8a
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-02-17 22:56:37 +0200

    iteration 2 passed

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,26 +1,34 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.io.*;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
+    private final String fileName = "tasks.csv";
 
     public TaskManager() {
         // Initialize tasks list
         tasks = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(File_name))) {
+        File tasksFile = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 tasks.add(line);
             }
         } catch (IOException e) {
-            System.err.println("Error while loading tasks: " + e.getMessage());
+            if (!tasksFile.exists()){
+                try{
+                    tasksFile.createNewFile();
+                } catch (IOException ex) {
+                    System.err.println("Error While creating the file: " +ex.getMessage());
+                }
+            }
         }
     }
 
     public void addTask(String task) {
        // throw new UnsupportedOperationException("Implement this method!");
         tasks.add(task);
-        exit();
     }
 
     public List<String> listTasks() {
@@ -35,8 +43,7 @@ public class TaskManager {
     public void exit() {
         // leave for iteration 2
         System.out.println("Tasks saved. Exiting..." );
-        System.exit(0);
-         try (BufferedWriter writer = new BufferedWriter(new FileWriter(File_name))) {
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             for (String task : tasks) {
                 writer.write(task);
                 writer.newLine();
